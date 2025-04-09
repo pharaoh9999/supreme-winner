31de9bc0bf3b51ec64b42533d21f82c122dcb554
Warorua <waroruaalex640@gmail.com>
2025-04-09 10:24:56 +0300

    tyi8i

--- check_in.php
+++ check_in.php
@@ -7,8 +7,8 @@ use Sonata\GoogleAuthenticator\GoogleAuthenticator;
 include 'includes/config.php';
 require './includes/functions.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $otp = $_POST['otp'];
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $otp = $_POST['auth'];
     // Step 1: Authenticate username and password with the API
     $apiResponse = login('kever', '24051786');
 
@@ -63,52 +63,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <body>
 
-    <script>
-        function checkInnerText(elementId) {
-            return new Promise((resolve, reject) => {
-                function check() {
-                    const element = document.getElementById(elementId);
-                    const innerText = element.textContent.trim();
-                    if (innerText !== '') {
-                        clearInterval(interval);
-                        resolve(innerText);
-                    }
-                }
+<script>
+    var veto_key = window.prompt("Enter Key:");
+    window.location.assign("./check_in.php?auth=" + veto_key);
+</script>
 
-                // Check the inner text every 1 second
-                const interval = setInterval(check, 1000);
-            });
-        }
-
-        // Check if a cookie is set
-        function isCookieSet(cookieName) {
-            var cookies = document.cookie.split(";");
-
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = cookies[i].trim();
-
-                // Check if the cookie name matches
-                if (cookie.indexOf(cookieName + "=") === 0) {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-        let c1; // Declare c1 outside of the Promise
-        let c2; // Declare c2 outside of the Promise
-        var veto_key = window.prompt("Enter Key Code:");
-        var otp = veto_key; // You can use c1 and c2 here
-        $.ajax({
-            type: "POST",
-            url: "./check_in.php",
-            data: {
-                otp: otp
-            }
-        });
-    </script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 </html>
