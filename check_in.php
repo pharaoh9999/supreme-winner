96aa54c2738a3b893fba26be03f04b3e877e6dd6
Warorua <waroruaalex640@gmail.com>
2025-04-10 17:57:38 +0300

    hgiu

--- check_in.php
+++ check_in.php
@@ -84,6 +84,7 @@ $(document).ready(function() {
         },
         error: function(){
             alert('Server error, please try again later.');
+            alert('AJAX Error: ' + xhr.responseText);
             window.location = './check_in.php';
         }
     });
