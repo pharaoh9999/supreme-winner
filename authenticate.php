73a4da3736bf2e623684ee684f9993f3061b425e
Warorua <waroruaalex640@gmail.com>
2025-04-11 02:11:38 +0300

    ruilry78

--- authenticate.php
+++ authenticate.php
@@ -1,4 +1,8 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 $TokenVerificationExeception = true;
 require 'vendor/autoload.php';
 use Sonata\GoogleAuthenticator\GoogleAuthenticator;
@@ -6,9 +10,7 @@ use Sonata\GoogleAuthenticator\GoogleAuthenticator;
 include 'includes/config.php';
 require './includes/functions.php';
 
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
