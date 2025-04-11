1b4edd84e23cf703294f630bca5ddcba4321e296
Warorua <waroruaalex640@gmail.com>
2025-04-11 04:41:52 +0300

    rhrtyjhtj

--- ajax/fetch_paid.php
+++ ajax/fetch_paid.php
@@ -1,15 +1,24 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 require '../includes/conn.php';
 
 header('Content-Type: application/json');
 
 $pdo = new AutoConn();
 $conn = $pdo->open();
+if(isset($_SESSION['id'])){
+$user_raw_id = $_SESSION['id']; // Replace with dynamic phone from session if needed
 
-$client_phone = "254700000000"; // Replace with dynamic phone from session if needed
+}else{
+    $user_raw_id = '666'; // Replace with dynamic phone from session if needed
 
-$stmt = $conn->prepare("SELECT number_plate, created_at FROM transactions WHERE client_phone = ? AND status = 'completed' ORDER BY created_at DESC LIMIT 10");
-$stmt->execute([$client_phone]);
+}
+
+$stmt = $conn->prepare("SELECT number_plate, created_at FROM transactions WHERE user_id = ? AND status = 'completed' ORDER BY created_at DESC LIMIT 10");
+$stmt->execute([$user_raw_id]);
 $results = $stmt->fetchAll();
 
 echo json_encode($results);
